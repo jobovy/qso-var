1fc17d48cc0edd1a117b0e80f70703946e5c2b13
Jo Bovy <bovy@ias.edu>
2012-10-12 16:44:19 -0400

    calculate S3

---
+++ py/skewQSO.py
@@ -0,0 +1,157 @@
+import sys
+import os, os.path
+import numpy as nu
+import cPickle as pickle
+from optparse import OptionParser
+import signal
+from varqso import VarQso, LCmodel
+from fitQSO import save_pickles, QSOfilenames
+_DEBUG=True
+_ERASESTR= "                                                                                "
+def skewQSO(parser):
+    (options,args)= parser.parse_args()
+    if len(args) == 0:
+        parser.print_help()
+        return
+    savefilename= args[0]
+    if os.path.exists(savefilename):
+        savefile= open(savefilename,'rb')
+        skews= pickle.load(savefile)
+        gaussskews= pickle.load(savefile)
+        type= pickle.load(savefile)
+        band= pickle.load(savefile)
+        mean= pickle.load(savefile)
+        taus= pickle.load(savefile)      
+        savefile.close()
+    else:
+        skews= {}
+        gaussskews= {}
+        type= options.type
+        mean= options.mean
+        band= options.band
+        taus= nu.arange(options.dtau,options.taumax,options.dtau)
+    if os.path.exists(options.fitsfile):
+        fitsfile= open(options.fitsfile,'rb')
+        params= pickle.load(fitsfile)
+        fitsfile.close()
+    else:
+        raise IOError("--fitsfile (or -f) has to be set to the file holding the best-fits")
+    if options.star:
+        dir= '../data/star/'
+    elif options.nuvx:
+        dir= '../data/nuvx/'
+    elif options.nuvxall:
+        dir= '../data/nuvx_all/'
+    elif options.uvx:
+        dir= '../data/uvx/'
+    elif options.rrlyrae:
+        dir= '../data/rrlyrae/'
+    else:
+        dir= '../data/s82qsos/'
+    qsos= QSOfilenames(dir=dir)
+    savecount= 0
+    count= len(skews)
+    for qso in qsos:
+        key= os.path.basename(qso)
+        if skews.has_key(key):
+            continue
+        try:
+            if int(key[5:7]) != options.rah and options.rah != -1:
+                continue
+        except ValueError:
+            if options.rah == -2 or options.rah == -1:
+                pass
+            else:
+                print "Skipping ValueError "+key
+                continue
+        sys.stdout.write('\r'+_ERASESTR+'\r')
+        sys.stdout.flush()
+        sys.stdout.write('\rWorking on %s: %s\r' % (str(count),key))
+        sys.stdout.flush()
+        v= VarQso(qso)
+        if v.nepochs(band) < 20:
+            #print "This object does not have enough epochs ..."
+            continue
+        #Set best-fit
+        v.LCparams= params[key]
+        v.LC= LCmodel(trainSet=v._build_trainset(band),type=type,mean=mean)
+        v.LCtype= type
+        v.LCmean= mean
+        v.fitband= band
+        #Now compute skew and Gaussian samples
+        thisskew= v.skew(taus,band)
+        thisgaussskews= nu.zeros((options.nsamples,len(taus)))
+        for ii in range(options.nsamples):
+            #First re-sample
+            o= v.resample(v.mjd[band],band=band,noconstraints=True)
+            o.LCparams= v.LCparams
+            o.LC= v.LC
+            o.fitband= v.fitband
+            o.LCtype= v.LCtype
+            o.LCmean= v.LCmean
+            thisgaussskews[ii,:]= o.skew(taus,band)
+        skews[key]= thisskew
+        gaussskews[key]= thisgaussskews
+        savecount+= 1
+        if savecount == options.saveevery:
+            print "Saving ..."
+            save_pickles(skews,gaussskews,type,band,mean,taus,savefilename)
+            savecount= 0
+        count+= 1
+    sys.stdout.write('\r'+_ERASESTR+'\r')
+    sys.stdout.flush()
+    save_pickles(skews,gaussskews,type,band,mean,taus,savefilename)
+    print "All done"
+
+def get_options():
+    usage = "usage: %prog [options] <savefilename>\n\nsavefilename= name of the file that the samples will be saved to"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-b","--band",dest='band',default='r',
+                      help="band(s) to sample")
+    parser.add_option("-t","--type",dest='type',default='powerlawSF',
+                      help="Type of model to sample (powerlawSF, powerlawSFratios, or DRW)")
+    parser.add_option("--mean",dest='mean',default='zero',
+                      help="Type of mean to sample (zero, const)")
+    parser.add_option("-f","--fitsfile",dest='fitsfile',
+                      default=None,
+                      help="File that holds the best-fits")
+    parser.add_option("-n","--nsamples",dest='nsamples',
+                      default=100,type='int',
+                      help="Number of samples to take")
+    parser.add_option("--saveevery",dest='saveevery',type='int',
+                      default=10,
+                      help="Save every --saveevery iterations")
+    parser.add_option("--rah",dest='rah',type='int',
+                      default=-1,
+                      help="RA hour to consider (-1: all, -2: ValueError)")
+    parser.add_option("--star",action="store_true", dest="star",
+                      default=False,
+                      help="Sample stars")
+    parser.add_option("--nuvx",action="store_true", dest="nuvx",
+                      default=False,
+                      help="Sample nUVX sample")
+    parser.add_option("--uvx",action="store_true", dest="uvx",
+                      default=False,
+                      help="Sample UVX sample")
+    parser.add_option("--nuvxall",action="store_true", dest="nuvxall",
+                      default=False,
+                      help="Sample nUVX_all sample")
+    parser.add_option("--rrlyrae",action="store_true", dest="rrlyrae",
+                      default=False,
+                      help="Sample RR Lyrae sample")
+    parser.add_option("--resampled",action="store_true", dest="resampled",
+                      default=False,
+                      help="Objects are 'resampled': stored in sav-file")
+    parser.add_option("-i","--infile",dest='infile',
+                      default=None,
+                      help="Input file if --resampled")
+    parser.add_option("--dtau",dest='dtau',
+                      default=3,type='float',
+                      help="lag spacing")
+    parser.add_option("--taumax",dest='taumax',
+                      default=150,type='float',
+                      help="lag spacing")
+    return parser
+
+if __name__ == '__main__':
+    skewQSO(get_options())
