e988640cb3e9f6e827e8b7f601c3eb9c3ea2bd1e
Jo Bovy <bovy@ias.edu>
2012-10-22 18:08:51 -0400

    add variables

--- py/submit_qsub_mpirun_i_100.sh
+++ py/submit_qsub_mpirun_i_100.sh
@@ -5,8 +5,10 @@
 #$ -V
 #$ -R y
 #$ -r y
+band=i
+nsamples=100
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64
 export MPI=/usr/local/openmpi/gcc/x86_64
 export PATH=${MPI}/bin:${PATH}
 export LD_LIBRARY_PATH=${MPI}/lib
-mpirun -x PYTHONPATH /home/bovy/local/bin/python skewQSOCluster.py ../skew/powerlawSF_constmean_i_skew.sav -b i -t powerlawSF --mean=const -f ../fits/powerlawSF_constmean_i.sav -n 100 --saveevery=5 --split=250
+mpirun -x PYTHONPATH /home/bovy/local/bin/python skewQSOCluster.py ../skew/powerlawSF_constmean_${band}_skew.sav -b ${band} -t powerlawSF --mean=const -f ../fits/powerlawSF_constmean_${band}.sav -n ${nsamples} --saveevery=5 --split=250
