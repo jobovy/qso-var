c4edf2cbc3becc56a3322cb5a3829fc0b6e37c3e
Jo Bovy <bovy@ias.edu>
2012-10-29 10:44:10 -0400

    changes for 0957

--- py/skew0957.py
+++ py/skew0957.py
@@ -20,22 +20,50 @@ def skew0957(parser):
     else:
         vA= VarQso('../data/L0957-A_%s.fits' % options.band,band=options.band)
         vB= VarQso('../data/L0957-B_%s.fits' % options.band,band=options.band)
-    #Fit for means
-    vA.fit(options.band,mean='const')
-    vB.fit(options.band,mean='const')
+    if not options.fitsfile is None and os.path.exists(options.fitsfile):
+        fitsfile= open(options.fitsfile,'rb')
+        paramsA= pickle.load(fitsfile)
+        paramsB= pickle.load(fitsfile)
+        paramsAB= pickle.load(fitsfile)
+        fitsfile.close()
+        vA.LCparams= paramsA
+        vB.LCparams= paramsB
+        vA.LCtype='powerlawSF'
+        vB.LCtype='powerlawSF'
+        vA.LCmean= 'const'
+        vB.LCmean= 'const'
+        vA.fitband= options.band
+        vB.fitband= options.band
+    else:
+        #Fit for means
+        print "Fitting SF for both images ..."
+        vA.fit(options.band,mean='const')
+        vB.fit(options.band,mean='const')
     #Load into single new VarQso
-    newm= list(vA.m[options.band]-vA.Lcparams['m'])
+    newm= list(vA.m[options.band]-vA.LCparams['m'])
     newerrm= list(vA.err_m[options.band])
     newmjd= list(vA.mjd[options.band])
-    newm.extend(newm.extend(list(vB.m['g']+nu.mean(vA.m[options.band])
-                                 -nu.mean(vB.m[options.band])-vB.LCparams['m'])))
+    newm.extend(list(vB.m[options.band]+nu.mean(vA.m[options.band])
+                                 -nu.mean(vB.m[options.band])-vB.LCparams['m']))
     newerrm.extend(list(vB.err_m[options.band]))
     newmjd.extend(list(vB.mjd[options.band]-417./365.25))#shift lagged B
-    v= varqso.VarQso(newmjd,newm,newerrm,band='g',medianize=False)
-    v.fit(options.band)
+    v= VarQso(newmjd,newm,newerrm,band=options.band,medianize=False)
+    if not options.fitsfile is None and os.path.exists(options.fitsfile):
+        v.LCparams= paramsAB
+        v.LCtype='powerlawSF'
+        v.LCmean= 'zero'
+        v.fitband= options.band
+        v.LC= LCmodel(trainSet=v._build_trainset(options.band),
+                      type=v.LCtype,mean=v.LCmean,
+                      init_params=paramsAB)
+    else:
+        v.fit(options.band)
+    if not options.fitsfile is None and not os.path.exists(options.fitsfile):
+        save_pickles(options.fitsfile,vA.LCparams,vB.LCparams,v.LCparams)
     taus= nu.arange(1.,201.,1.)/365.25
-    thisskew= v.skew(taus,options.band)
-    thisgaussskews= nu.zeros((len(taus)))
+    thisskew= v.skew(taus,options.band,duration=0.7)
+    thisgaussskews= nu.zeros((options.nsamples,len(taus)))
+    print "Calculating simulated skews ..."
     for ii in range(options.nsamples):
         #First re-sample
         redshift= 1.41
@@ -52,8 +80,18 @@ def skew0957(parser):
             o.LCparams['gamma']= 1.
             o.LCparams['logA']= o.LCparams['logA']\
                 +nu.log(0.05**v.LCparams['gamma']/0.05)
-            thisgaussskews[ii,:]= o.skew(taus,options.band)
-    save_pickles(savefilename,skews,gaussskews,None,options.band,None,taus)
+            o.LCmean= 'zero' #bc we remove the mean when resampling wedge
+            #Set up LC with correct params
+            o.LC= LCmodel(trainSet=o._build_trainset(options.band),
+                          type=o.LCtype,mean=o.LCmean,
+                          init_params=o.LCparams)
+        thisgaussskews[ii,:]= o.skew(taus,options.band,duration=0.7)
+    skews= {}
+    gaussskews= {}
+    skews['0957']= thisskew
+    gaussskews['0957']= thisgaussskews
+    save_pickles(savefilename,skews,gaussskews,
+                 None,options.band,None,taus)
     return None
 
 def get_options():
@@ -64,6 +102,9 @@ def get_options():
     parser.add_option("-n","--nsamples",dest='nsamples',
                       default=100,type='int',
                       help="Number of samples to take")
+    parser.add_option("-f","--fitsfile",dest='fitsfile',
+                      default=None,
+                      help="File that holds the best-fits")
     parser.add_option("--dtau",dest='dtau',
                       default=1,type='float',
                       help="lag spacing")
